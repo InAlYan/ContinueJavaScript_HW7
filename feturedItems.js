8c28271cd939f25d63deacbba631427352ca9ad2
InAlYan <sayanvic@mail.ru>
2024-10-13 18:20:27 +0700

    Continue JavaScript HW7

---
+++ feturedItems.js
@@ -0,0 +1,52 @@
+const feturedItems = `
+[
+    {
+        "imageSrc": "./img/grid_card_1.png",
+        "imageAlt": "карточка 1",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "imageSrc": "./img/grid_card_2.png",
+        "imageAlt": "карточка 2",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "imageSrc": "./img/grid_card_3.png",
+        "imageAlt": "карточка 3",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "imageSrc": "./img/grid_card_4.png",
+        "imageAlt": "карточка 4",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "imageSrc": "./img/grid_card_5.png",
+        "imageAlt": "карточка 5",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    },
+    {
+        "imageSrc": "./img/grid_card_6.png",
+        "imageAlt": "карточка 6",
+        "url": "./product.html",
+        "urlText": "ELLERY X&nbsp;M&rsquo;O CAPSULE",
+        "info": "Known for her sculptural takes on&nbsp;traditional tailoring, Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+        "price": 52.00
+    }
+]
+`
---
+++ shoppingCards.js
@@ -0,0 +1,64 @@
+const shoppingCards = `
+    [
+        {
+            "imageSrc": "./img/card_1.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "MANGO PEOPLE T-SHIRT",
+            "price": 300,
+            "color": "Dark-blue",
+            "size": "XI",
+            "quantity": 1
+        },
+        {
+            "imageSrc": "./img/card_2.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "ELLERY X M'O CAPSULE",
+            "price": 52,
+            "color": "Black",
+            "size": "X",
+            "quantity": 1
+        },
+        {
+            "imageSrc": "./img/card_3.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "MANGO PEOPLE T-SHIRT",
+            "price": 200,
+            "color": "Fiolet-black",
+            "size": "XII",
+            "quantity": 1
+        },
+        {
+            "imageSrc": "./img/card_4.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "ELLERY X M'O CAPSULE",
+            "price": 152,
+            "color": "Light-blue",
+            "size": "X",
+            "quantity": 1
+        },
+        {
+            "imageSrc": "./img/card_5.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "MANGO PEOPLE T-SHIRT",
+            "price": 90,
+            "color": "Grey-blue",
+            "size": "XI",
+            "quantity": 1
+        },
+        {
+            "imageSrc": "./img/card_6.png",
+            "imageAlt": "карточка товара",
+            "url": "./product.html",
+            "urlText": "ELLERY X M'O CAPSULE",
+            "price": 70,
+            "color": "Grey",
+            "size": "X",
+            "quantity": 1
+        }
+    ]
+`
